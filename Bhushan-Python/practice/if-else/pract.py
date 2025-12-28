a8514f67d7d53e531dc975f842d573139ebf223e
Bhushan Jatgade <bhushanjatgade@gmail.com>
2025-12-29 00:53:04 +0530

    Update pract.py

--- Bhushan-Python/practice/if-else/pract.py
+++ Bhushan-Python/practice/if-else/pract.py
@@ -8,7 +8,7 @@ extracheese = input("Do you want extra cheese ? Y or N: ").lower()
 
 price = 0
 
-bill = 0
+
 
 if size == "s":
     price = 15
@@ -16,17 +16,18 @@ elif size == "m":
     price = 20
 elif size == 'l':
     price = 25 
-    
-    if papperoni == "y":
-        if size == "s":
-            bill = price + 2
-        else:
-            bill = price + 3
-        if extracheese == "y":
-            bill = price + 1
-
-total = bill
 
 
-print(f"Your final bill is:{total}")
+if papperoni == "y":
+    if size == "s":
+        price += 2
+    else:
+        price += 3
 
+if extracheese == "y":
+    price  += 1
+
+total = price
+
+
+print(f"Your final bill is: ${total}")
